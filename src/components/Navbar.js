1f61bb1dbae862eec127bc81207de8d5fd106237
Byron Wang <byronwang123919@gmail.com>
2022-07-15 00:54:26 -0700

    finish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
-import { FiAlignJustify } from 'react-icons/fi';
+import { FaAlignJustify } from 'react-icons/fa';
+import logo from '../assets/images/logo.svg';
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <nav>
-      <FiAlignJustify />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div class="nav-center">
+        <div class="nav-header">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="simply recipes"
+            />
+          </Link>
+          <button
+            className="nav-btn"
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            <FaAlignJustify />
+          </button>
+        </div>
+        <div class={show ? 'nav-links show-links' : 'nav-links'}>
+          <Link
+            to="/"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            Recipes
+          </Link>
+          <Link
+            to="/tags"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            Tags
+          </Link>
+          <Link
+            to="/about"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            About
+          </Link>
+          <div class="nav-link contact-link">
+            <Link
+              to="/contact"
+              className="btn"
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
