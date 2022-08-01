cc0231f7bf030228f6b128fc5484560db21c844b
Byron Wang <byronwang123919@gmail.com>
2022-07-31 23:56:33 -0700

    added tags list implementation

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,9 +1,27 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 import setupTags from '../utils/setupTags';
+import { Link } from 'gatsby';
 
 const TagsList = ({ recipes }) => {
   const newTags = setupTags(recipes);
-  return <div>This is the tagslist</div>;
+  return (
+    <div className="tag-container">
+      <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag;
+          return (
+            <Link
+              to={`/${text}`}
+              key={index}
+            >
+              {text} ({value})
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TagsList;
